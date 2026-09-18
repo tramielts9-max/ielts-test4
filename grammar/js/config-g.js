06374260dd8f42e75efc8783374b8a34966669d8
tramielts9-max <tramielts9@gmail.com>
2026-09-18 21:22:34 +0700

    Create config-g.js

---
+++ grammar/js/config-g.js
@@ -0,0 +1,8 @@
+export const GRAMMAR_CONFIG = {
+  STORAGE_KEYS: {
+    USER_PROGRESS: "ielts_grammar_progress_v1",
+    STUDENT_NAME: "ielts_student_name",
+    STUDENT_EMAIL: "ielts_student_email"
+  },
+  PASSING_SCORE_PERCENT: 80
+};
