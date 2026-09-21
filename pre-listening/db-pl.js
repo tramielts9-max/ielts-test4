7e356aac50dd674eb83439069e897e97e95704c7
tramielts9-max <tramielts9@gmail.com>
2026-09-21 16:09:30 +0700

    Create db-pl.js

---
+++ pre-listening/db-pl.js
@@ -0,0 +1,47 @@
+const DB_NAME = "PreListeningHomeworkDB";
+const STORE_NAME = "ListeningSubmissions";
+let db = null;
+
+export function initDB() {
+  return new Promise((resolve, reject) => {
+    const req = indexedDB.open(DB_NAME, 1);
+    req.onupgradeneeded = (e) => {
+      const d = e.target.result;
+      if (!d.objectStoreNames.contains(STORE_NAME)) {
+        d.createObjectStore(STORE_NAME, { keyPath: "id" });
+      }
+    };
+    req.onsuccess = (e) => { db = e.target.result; resolve(db); };
+    req.onerror = (e) => reject(e);
+  });
+}
+
+export function getSubmission(id) {
+  return new Promise((resolve) => {
+    const tx = db.transaction([STORE_NAME], "readonly");
+    const store = tx.objectStore(STORE_NAME);
+    const req = store.get(id);
+    req.onsuccess = () => resolve(req.result || null);
+    req.onerror = () => resolve(null);
+  });
+}
+
+export function saveSubmission(data) {
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction([STORE_NAME], "readwrite");
+    const store = tx.objectStore(STORE_NAME);
+    const req = store.put(data);
+    req.onsuccess = () => resolve();
+    req.onerror = (e) => reject(e);
+  });
+}
+
+export function getAllSubmissions() {
+  return new Promise((resolve) => {
+    const tx = db.transaction([STORE_NAME], "readonly");
+    const store = tx.objectStore(STORE_NAME);
+    const req = store.getAll();
+    req.onsuccess = () => resolve(req.result || []);
+    req.onerror = () => resolve([]);
+  });
+}
