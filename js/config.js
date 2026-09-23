5ea9a353750585df21a6a055e49e80b713efd99a
tramielts9-max <tramielts9@gmail.com>
2026-09-23 09:06:45 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
  */
 export const CONFIG = {
   AI_AND_SHEET_URL: "https://script.google.com/macros/s/AKfycby7vRFXq_YhjIEq4kN-8NLRFw2sj-7VkVEmTw6IkNkPmidEPnPtxtNkSE-HKfn5mAPfbw/exec",
-  DRIVE_STORAGE_URL: "https://script.google.com/macros/s/AKfycbx5HRPHr75RLlcuXvcn1QSTmsLszIhYH6cDrKiGZS4RCoxa0l3NJF4dKWplI1sVKVoCYg/exec",
+  DRIVE_STORAGE_URL: "https://script.google.com/macros/s/AKfycbyNErQQFdciAQM0k9KUrACtpX7rxKkopjChYAC2Ubwj5MGzFOeekDEGs8C1n7P9cNR6vg/exec",
   STORAGE_KEYS: {
     STUDENT_NAME: "ielts_student_name",
     STUDENT_EMAIL: "ielts_student_email",
